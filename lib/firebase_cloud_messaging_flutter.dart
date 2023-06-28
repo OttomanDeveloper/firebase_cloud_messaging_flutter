798f2e3608ef714f746855d4922940cf5a219cf4
dsyrstad <dsyrstad@vscorp.com>
2023-06-28 15:41:56 -0500

    Fixes issues #1 and #2.

--- lib/firebase_cloud_messaging_flutter.dart
+++ lib/firebase_cloud_messaging_flutter.dart
@@ -2,12 +2,13 @@
 ///https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages
 library firebase_cloud_messaging_flutter;
 
-export 'package:firebase_cloud_messaging_flutter/src/logic/send.dart';
-export 'package:firebase_cloud_messaging_flutter/src/logic/message.dart';
-export 'package:firebase_cloud_messaging_flutter/src/logic/notification.dart';
-export 'package:firebase_cloud_messaging_flutter/src/logic/android.config.dart';
-export 'package:firebase_cloud_messaging_flutter/src/logic/android.notification.dart';
-export 'package:firebase_cloud_messaging_flutter/src/logic/firebase.service.model.dart';
-export 'package:firebase_cloud_messaging_flutter/src/firebase.cloud.messaging.server.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/firebase.cloud.messaging.server.dart';
+export 'src/logic/android.config.dart';
+export 'src/logic/android.notification.dart';
+export 'src/logic/apns.config.dart';
+export 'src/logic/fcm.options.dart';
+export 'src/logic/firebase.service.model.dart';
+export 'src/logic/message.dart';
+export 'src/logic/notification.dart';
+export 'src/logic/send.dart';
+export 'src/logic/webpush.config.dart';
--- lib/src/firebase.cloud.messaging.server.dart
+++ lib/src/firebase.cloud.messaging.server.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
+
 import 'package:firebase_cloud_messaging_flutter/firebase_cloud_messaging_flutter.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart';
 
 /// Send Firebase Cloud Messages directly from your Dart or Flutter App
 class FirebaseCloudMessagingServer {
-  ///Firebase console:
-  ///open settings > Service Accounts
-  ///Generate New Private Key > Generate Key
+  /// Firebase console:
+  /// open settings > Service Accounts
+  /// Generate New Private Key > Generate Key
   /// Open Your Json file and Copy all the code.
   final Map<String, dynamic> firebaseServiceCredentials;
 
-  ///Caches authCredentials(up to 1 hour)
+  /// Caches authCredentials(up to 1 hour)
   final bool cacheAuth;
 
   AccessCredentials? accessCredentials;
@@ -22,31 +23,31 @@ class FirebaseCloudMessagingServer {
   });
 
   Future<AccessCredentials?> performAuth() async {
-    /// Get Service Account Credentials from Given Map
+    // Get Service Account Credentials from Given Map
     final ServiceAccountCredentials accountCredentials =
         ServiceAccountCredentials.fromJson(
       firebaseServiceCredentials,
     );
 
-    /// We only required messaging scope to send messages
+    // We only required messaging scope to send messages
     final List<String> scopes = <String>[
       'https://www.googleapis.com/auth/firebase.messaging',
     ];
 
-    /// Create a instance of HTTP Client to perform server request
+    // Create a instance of HTTP Client to perform server request
     final Client client = Client();
 
-    /// Get Access to Requested Scop via service account details
+    // Get Access to Requested Scop via service account details
     accessCredentials = await obtainAccessCredentialsViaServiceAccount(
       accountCredentials,
       scopes,
       client,
     );
 
-    /// Close Http server request
+    // Close Http server request
     client.close();
 
-    /// send acess credentials
+    // send acess credentials
     return accessCredentials;
   }
 
@@ -74,11 +75,11 @@ class FirebaseCloudMessagingServer {
     if (accessCredentials == null ||
         DateTime.now().isAfter(accessCredentials!.accessToken.expiry) ||
         !cacheAuth) {
-      /// perform access token request
+      // perform access token request
       await performAuth();
     }
 
-    /// Send Message Request and Save it's response
+    // Send Message Request and Save it's response
     final Response response = await Client().post(
       Uri.parse(
         'https://fcm.googleapis.com/v1/projects/${FirebaseServiceModel.fromJson(firebaseServiceCredentials).projectID}/messages:send',
@@ -90,13 +91,13 @@ class FirebaseCloudMessagingServer {
       body: json.encode(sendObject.toJson()),
     );
 
-    /// Get request response
+    // Get request response
     final bool successful = response.statusCode == 200;
 
-    /// Print Request response
-    print('successful: $successful');
+    // Print Request response
+    // print('successful: $successful');
 
-    /// Get Server Request result from Request Body [response.body]
+    // Get Server Request result from Request Body [response.body]
     final ServerResult serverResult = ServerResult(
       successful: successful,
       statusCode: response.statusCode,
@@ -106,10 +107,10 @@ class FirebaseCloudMessagingServer {
           : FirebaseMessage(),
     );
 
-    /// Print Server Response in console
-    print(serverResult);
+    // Print Server Response in console
+    // print(serverResult);
 
-    /// return server responses
+    // return server responses
     return serverResult;
   }
 }
